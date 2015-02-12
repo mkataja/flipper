a7ed754c4381ef76866b7c6bcb1f2739bd78c356
mkataja <mkataja@gmail.com>
2015-02-12 20:31:27 +0200

    !roll improvements

--- src/commands/builtincommands.py
+++ src/commands/builtincommands.py
@@ -134,7 +134,18 @@ class RealWeatherCommand(Command):
 class RollCommand(Command):
     helpstr = "Käyttö: speksaa heitettävä noppa muodossa AdX (esim 2d6)"
     
+    rolling = False
+    
     def handle(self, message):
+        if RollCommand.rolling:
+            message.reply_to("Yksi heitto kerrallaan")
+            return
+        
+        RollCommand.rolling = True
+        self.roll(message)
+        RollCommand.rolling = False
+    
+    def roll(self, message):
         matches = re.match("([1-9][0-9]*)d([1-9][0-9]*)", message.params.strip())
         
         if matches is None:
@@ -148,16 +159,18 @@ class RollCommand(Command):
             message.reply_to("Liikaa noppia")
             return
         
-        if num_faces > 10000:
+        if not 1 < num_faces <= 10000:
             message.reply_to("Nigga please, {}...".format(num_faces))
             return
         
-        rolls = [random.randint(1, num_faces) for p in range(0, num_dice)]
+        rolls = [random.randint(1, num_faces) for i in range(num_dice)]
         
         if num_dice > 4:
             message.reply_to("Heitit: {}. Heittojen summa: {}"
                              .format(", ".join(map(str, rolls)),
                                      sum(rolls)))
+        elif num_dice == 1:
+            message.reply_to("Heitit: {}".format(rolls[0]))
         else:
             for roll in rolls:
                 message.reply_to("Heitit: {}...".format(roll))
