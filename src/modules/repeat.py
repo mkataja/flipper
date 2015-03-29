03ad00c05ca2c434a8d19ecfeaadd3ffc5d0792b
mkataja <mkataja@gmail.com>
2015-03-29 18:50:21 +0300

    Add a lock in RepeatModule

--- src/modules/repeat.py
+++ src/modules/repeat.py
@@ -1,15 +1,19 @@
+import threading
+
 from modules.module import Module
 
 
 class RepeatModule(Module):
     last = {}
+    updating = threading.Lock()
     
     def on_pubmsg(self, connection, event):
         target = event.target
         message = event.arguments[0].strip()
-        last = self.last.get(target, None)
-        if message == last:
-            self._bot.safe_privmsg(target, message)
-            self.last[target] = None
-        else:
-            self.last[target] = message
+        with RepeatModule.updating:
+            last = self.last.get(target, None)
+            if message == last:
+                self._bot.safe_privmsg(target, message)
+                self.last[target] = None
+            else:
+                self.last[target] = message
