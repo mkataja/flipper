2ad5b5f96049cf431cbe6f6b2b613e54ffa49b86
mkataja <mkataja@gmail.com>
2015-03-29 20:41:17 +0300

    Add patch to make irclib DelayedCommand use utcnow() instead of now()

--- src/patch.py
+++ src/patch.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 
 import irc
@@ -9,6 +10,7 @@ def patch_irclib():
     """
     logging.info("Patching irc library...")
     patch_client_reactor_sockets()
+    patch_schedule_delayedcommand_now()
     
 def patch_client_reactor_sockets():
     """
@@ -25,3 +27,14 @@ def patch_client_reactor_sockets():
                     if socket.fileno() >= 0
             ]
     irc.client.Reactor.sockets = sockets_new
+
+def patch_schedule_delayedcommand_now():
+    """
+    Have delayed commands use UTC to avoid DST problems
+    """
+    logging.info("  Patching schedule.DelayedCommand.now()")
+    @classmethod
+    def now_new(cls, tzinfo=None):
+        # Ignoring tzinfo because of UTC
+        return datetime.datetime.utcnow()
+    irc.schedule.DelayedCommand.now = now_new 
