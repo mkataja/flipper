912c48129f5fc6de1406575af529cf3e2d6cfa93
mkataja <mkataja@gmail.com>
2015-05-10 02:44:54 +0300

    Add generic Base for SQLAlchemy declarative tables

--- src/services/database.py
+++ src/services/database.py
@@ -1,9 +1,13 @@
 import contextlib
 import logging
+import re
 
 from sqlalchemy.engine import create_engine
+from sqlalchemy.ext.declarative.api import declarative_base, declared_attr
 from sqlalchemy.orm.scoping import scoped_session
 from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import Integer
 
 import config
 
@@ -11,6 +15,7 @@ import config
 def initialize():
     logging.info("Initializing database connection to " + config.DATABASE_URI)
     engine = create_engine(config.DATABASE_URI, pool_size=10)
+    FlipperBase.metadata.create_all(engine)
     global Session
     Session = scoped_session(sessionmaker(bind=engine))
 
@@ -22,3 +27,20 @@ def get_session():
     finally:
         logging.info("Removing SQLAlchemy session")
         Session.remove()
+        
+
+Base = declarative_base()
+
+class FlipperBase(Base):
+    __abstract__ = True
+    
+    _tablename = None
+    
+    @declared_attr
+    def __tablename__(self):
+        if self._tablename is None:
+            s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', self.__name__)
+            self._tablename = re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+        return self._tablename
+
+    id = Column(Integer, primary_key=True)
