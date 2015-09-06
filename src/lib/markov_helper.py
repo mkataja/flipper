3d8601e3105550d54cd9b37871ad1567fa8c3a07
mkataja <mkataja@gmail.com>
2015-09-06 20:27:19 +0300

    Markov: filter out some useless words and short sentences

--- src/lib/markov_helper.py
+++ src/lib/markov_helper.py
@@ -1,22 +1,31 @@
 import re
 
 from lib.iteration import previous_and_next
-from models.markov_entry import MarkovEntry
-from services import database
 from models.markov_corpus import MarkovCorpus
+from models.markov_entry import MarkovEntry
 from models.markov_word import MarkovWord
+from services import database
 
+
+WORD_MIN_LENGTH = 2
+WORD_MAX_LENGTH = 31
+
+
+def _is_valid_word(w):
+    return len(w) >= WORD_MIN_LENGTH and len(w) <= WORD_MAX_LENGTH
 
 def parse_markov_sentences(input_text):
     """
     Parses the input into properly sanitized sentences compliant with
     the bot's Markov chain implementation.
-    
-    Fairly trivial implementation: throws away most non-word
-    characters and splits sentences at [.!?].
     """
-    sanitized_text = re.sub('[^ .!?\w-]', '', input_text.lower())
-    sentences = re.split('[.!?]', sanitized_text)
+    url_regex = '(?:(?:https?:\/\/)|www\.)(?:(?:[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)|(?:[a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+)(?::[0-9]+)?(?:(?:\/|\?)[^ "]*[^ ,;\.:">)])?'
+    input_text = re.sub(url_regex, '', input_text, re.IGNORECASE)
+    input_text = re.sub('[^ .!?\w-]', '', input_text)
+    input_text = input_text.lower()
+    sentences = [[w for w in s.split() if _is_valid_word(w)]
+                 for s in re.split('[.!?]', input_text)]
+    sentences = [s for s in sentences if len(s) > 1]
     return sentences
 
 def get_or_create_corpus(corpus_name):
@@ -46,7 +55,7 @@ def insert_markov_sentences(sentences, corpus_name, text_identifier):
         corpus_id = get_or_create_corpus(corpus_name)
         for i, sentence in enumerate(sentences):
             sentence_identifier = '{}_{}'.format(text_identifier, i)
-            word_ids = [get_or_create_word(word) for word in sentence.split()]
+            word_ids = [get_or_create_word(word) for word in sentence]
             for previous, current, following in previous_and_next(word_ids):
                 entry = MarkovEntry()
                 entry.corpus_id = corpus_id
@@ -55,4 +64,4 @@ def insert_markov_sentences(sentences, corpus_name, text_identifier):
                 entry.prev1_id = current
                 entry.next_id = following
                 session.add(entry)
-        session.commit()
+            session.commit()
