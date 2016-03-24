953fd4637c97bf516dc92014cc51f489a4968d7c
mkataja <mkataja@gmail.com>
2016-03-24 11:21:18 +0200

    Enhance niiloisms

--- src/lib/niiloism.py
+++ src/lib/niiloism.py
@@ -2,7 +2,7 @@ import random
 
 
 def _weighted_choice(choices):
-    total = sum(w for c, w in choices)
+    total = sum(w for _, w in choices)
     r = random.uniform(0, total)
     upto = 0
     for c, w in choices:
@@ -40,26 +40,30 @@ def _pick_vowel_type():
 
 def _random_ending(last_vowel, vowels):
     return random.choice([
-        lambda l, v: "",
-        lambda l, v: "s",
-        lambda l, v: "l{}".format(l),
-        lambda l, v: "r{}".format(l),
-        lambda l, v: "r{}{}".format(l, l),
-        lambda l, v: "kk{}".format(l),
-        lambda l, v: "d{}r".format(l),
-        lambda l, v: "t{}s".format(l),
-        lambda l, v: "ts{}".format(l),
-        lambda l, v: "l{}{}n{}".format(l, l, random.choice(vowels)),
-        lambda l, v: "ll{}pp{}".format(l, random.choice(vowels)),
-    ])(last_vowel, vowels)
+            lambda l, v: "",
+            lambda l, v: "s",
+            lambda l, v: "l{}".format(l),
+            lambda l, v: "r{}".format(l),
+            lambda l, v: "r{}{}".format(l, l),
+            lambda l, v: "kk{}".format(l),
+            lambda l, v: "d{}r".format(l),
+            lambda l, v: "t{}s".format(l),
+            lambda l, v: "ts{}".format(l),
+            lambda l, v: "l{}{}n{}".format(l, l, random.choice(vowels)),
+            lambda l, v: "ll{}pp{}".format(l, random.choice(vowels)),
+        ])(last_vowel, vowels)
 
 def random_word():
     all_vowels = _pick_vowel_type()
-
     vowels = _random_vowels(all_vowels)
+
+    first_vowel = _weighted_choice([
+                                    [lambda v: v, 3],
+                                    [lambda v: "{}{}".format(v, v), 1],
+                                    ])(vowels[0])
     first_consonant = random.choice(['s', 'ss', 'p', 'pp', 'ps', 't', 'ts', ''])
 
-    header = _weighted_choice([['nj', 8], ['h', 1], ['p', 1]])
+    header = _weighted_choice([['nj', 8], ['tj', 2], ['h', 1], ['p', 1]])
     ender = _random_ending(vowels[2], all_vowels)
 
-    return "{}{}{}{}{}".format(header, vowels[0], first_consonant, vowels[1], ender)
+    return "{}{}{}{}{}".format(header, first_vowel, first_consonant, vowels[1], ender)
