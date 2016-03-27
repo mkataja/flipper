8f143b7a7878e387df67aa746019b2f55f87c7e3
mkataja <mkataja@gmail.com>
2016-03-27 16:13:03 +0300

    Implement IRC formatting functions (colors and bold)

---
+++ src/lib/irc_colors.py
@@ -0,0 +1,38 @@
+from enum import unique, Enum
+
+
+@unique
+class Color(Enum):
+    white = '00'
+    black = '01'
+    dblue = '02'
+    dgreen = '03'
+    red = '04'
+    dred = '05'
+    purple = '06'
+    orange = '07'
+    yellow = '08'
+    green = '09'
+    dcyan = '10'
+    cyan = '11'
+    blue = '12'
+    violet = '13'
+    dgrey = '14'
+    grey = '15'
+
+
+@unique
+class ControlCode(Enum):
+    bold = '\x02'
+    color = '\x03'
+    reset = '\x0f'
+
+
+def color(string, fg_color, bg_color=None):
+    color_code = "{}{}".format(ControlCode.color.value, fg_color.value)
+    if bg_color:
+        color_code = "{},{}".format(color_code, bg_color.value)
+    return "{}{}{}".format(color_code, string, ControlCode.reset.value)
+
+def bold(string):
+    return "{}{}{}".format(ControlCode.bold.value, string, ControlCode.reset.value)
