485eb4e5677bf8b3eefd3419c65b72c8ad9b29e0
mkataja <mkataja@gmail.com>
2016-10-06 20:53:57 +0300

    Also allow superuser with ~ in mask (as irc servers seem to have trouble with identd)

--- src/services/accesscontrol.py
+++ src/services/accesscontrol.py
@@ -3,4 +3,5 @@ import config
 
 def has_admin_access(nickmask):
     return (nickmask.nick == config.SUPERUSER_NICK and
-            nickmask.user == config.SUPERUSER_NAME)
+            (nickmask.user == config.SUPERUSER_NAME or
+             nickmask.user == "~{}".format(config.SUPERUSER_NAME)))
