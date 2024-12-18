152710286b5a5df57af072b52012c5be559881f5
Matias Katajamäki <mkataja@gmail.com>
2024-12-18 02:29:20 +0200

    Add build argument in start script

--- start.sh
+++ start.sh
@@ -5,5 +5,8 @@ set -e
 export FLIPPER_UID=$(id -u flipper)
 export FLIPPER_GID=$(id -g flipper)
 
-docker compose -f ./docker-compose-run.yaml up --build -d --no-recreate flipper
+if [ $1 = "build" ]; then
+  docker compose build
+fi
+docker compose up --build -d --no-recreate flipper
 
