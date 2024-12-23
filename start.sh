947264f0c3d72fd75da7c482087206035d8e5094
Matias Katajamäki <mkataja@gmail.com>
2024-12-23 03:03:35 +0200

    Add "build" argument to start.sh to force complete rebuild

--- start.sh
+++ start.sh
@@ -5,8 +5,9 @@ set -e
 export FLIPPER_UID=$(id -u flipper)
 export FLIPPER_GID=$(id -g flipper)
 
-if [ $1 = "build" ]; then
-  docker compose build
+if [[ $1 == "build" ]]; then
+  echo "Rebuild requested"
+  docker compose build --no-cache
 fi
-docker compose up --build -d --no-recreate flipper
+docker compose up --build --no-recreate -d flipper
 
